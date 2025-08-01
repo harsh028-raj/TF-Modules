19e36de649387da6d61e1ec4f54e2998b3800477
harsh028-raj <74181011+harsh028-raj@users.noreply.github.com>
2025-08-01 14:46:43 +0530

    Create main.tf
    VPC-Main

---
+++ vpc/main.tf
@@ -0,0 +1,73 @@
+Skip to content
+GitLab
+Why GitLab
+Pricing
+Explore
+Sign in
+Primary navigation
+Project
+C
+cicdTF
+
+Manage
+
+Plan
+
+Code
+
+Build
+
+Deploy
+
+Operate
+
+Monitor
+
+Analyze
+ 
+N4si
+cicdTF
+cicdtf
+vpc
+main.tf
+main.tf
+N4si's avatar
+Initial commit
+N4si authored Mar 1, 2024
+f6608a18
+main.tf
+745 B
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = {
+    Name = "vpc-1"
+  }
+}
+resource "aws_subnet" "pb_sn" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.pb_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "pb_1"
+  }
+}
+resource "aws_security_group" "pb_sg" {
+  name        = "my sg"
+  description = "SSH inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ext_ip]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.ext_ip]
+  }
+}
